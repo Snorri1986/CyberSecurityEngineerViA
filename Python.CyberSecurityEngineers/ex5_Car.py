2ade6282148668ca3c138d704047e4cb4f32a81b
Denys Shabelnyk <dionisiy1986@gmail.com>
2022-09-20 00:05:34 +0300

    add ex5. Class

---
+++ Python.CyberSecurityEngineers/ex5_Car.py
@@ -0,0 +1,52 @@
+# define Car class
+from contextlib import redirect_stderr
+
+class Car:
+    def __init__(self,color,power,type,price):
+        self.color = color
+        self.power = power
+        self.type = type
+        self.price = price
+# Getters
+    def getColor(self):
+        return self.color
+
+    def getPower(self):
+        return self.power
+
+    def getType(self):
+        return self.type
+
+    def getPrice(self):
+        return self.price
+
+    def run(self):
+        print("Car is driving now")
+
+    def stop(self):
+        print("Car is stoping now")
+
+# Setters
+    def setColor(self,newColor):
+        self.color = newColor 
+        
+    def setPower(self,newPower):
+        self.power = newPower
+
+    def setPrice(self,newPrice):
+        self.price = newPrice
+
+# Car in action
+vehicle = Car("black",1000,"sedan",23000)
+speed = input("Enter current speed")
+if int(speed) > 0:
+    vehicle.run()
+elif int(speed) == 0:
+    vehicle.stop()   
+else:
+    print("Unknown state")     
+# Change color of a car
+color = "red"
+vehicle.setColor(color)
+#Show result
+print("New car color is ",vehicle.getColor())
---
+++ Python.CyberSecurityEngineers/ex5_Person.py
@@ -0,0 +1,44 @@
+#define Person class
+class Person:
+    def __init__(self,name,surname,age,race):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.race = race
+
+ #Getters
+    def getName(self):
+        return self.name
+
+    def getSurname(self):
+        return self.surname
+
+    def getAge(self):
+        return self.age
+
+    def getRace(self):
+        return self.race
+
+#Setters
+    def setName(self,newName):
+        self.name = newName
+
+    def setSurname(self,newSurname):
+        self.surname = newSurname
+
+    def setAge(self,newAge):
+        self.age = newAge
+# Person in action
+man = Person("Denys","Shabelnyk",36,"White")
+woman = Person("Lucy","Ivanova",31,"White")
+# Change ages 
+setManAge = man.setAge(input("Enter age of a man:"))
+setWomanAge = woman.setAge(input("Enter age of a woman:"))
+manAge = int(man.getAge())
+womanAge = int(woman.getAge())
+if (manAge or womanAge) > 30:
+    print("We are adult")
+elif (manAge or womanAge) < 18:
+    print("We are teenagers")
+else:
+    print("Midlle ages")
