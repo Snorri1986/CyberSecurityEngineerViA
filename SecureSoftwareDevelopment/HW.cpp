b8d1e27ea7554e1fe0288f01476469caf1c372ed
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-03-28 23:34:46 +0200

    Austris report

---
+++ SecureSoftwareDevelopment/HW.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+	printf("Hello world from Assembly!");
+	return 0;
+}
---
+++ SecureSoftwareDevelopment/variable.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	int variable = 10;
+	printf("Variable: %d",variable);
+	return 0;
+}
