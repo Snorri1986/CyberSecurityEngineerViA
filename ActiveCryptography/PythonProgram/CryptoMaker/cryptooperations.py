397813857d0e97df6ac50443a1efee5c3b7e6a29
Denys Shabelnyk <dionisiy1986@gmail.com>
2023-04-04 23:21:19 +0200

    generated otp,continue working to convert from str to binary

--- ActiveCryptography/PythonProgram/CryptoMaker/cryptooperations.py
+++ ActiveCryptography/PythonProgram/CryptoMaker/cryptooperations.py
@@ -1,10 +1,6 @@
+import random
 def gen_random_key(n):
     """Generates a random key of bits (with 0s or 1s) of length n"""
-    A = 1
-    B = 0
-    size = n
-    key = []
-    for x in size :
-       if(x%2 == 0) : key.append(A)
-       else : key.append(B)
-    #TODO: loop does not work
+    key = [0] * (round(n/2)) + [1] * (round(n/2))
+    random.shuffle(key)
+    return key
--- ActiveCryptography/PythonProgram/CryptoMaker/files.py
+++ ActiveCryptography/PythonProgram/CryptoMaker/files.py
@@ -1,10 +1,11 @@
-import os
+import os,math
 from cryptooperations import gen_random_key
 
 # def showfilename(param1, param2, param3):
 #     print(param1, param2, param3)
 
 def encryptfile():
+    """Function for encrypt file"""
     print("Encryption............")
     print("Write the filename to encrypt with extension")
     file_to_encrypt = input()
@@ -13,10 +14,15 @@ def encryptfile():
     print("Write the filename for encrypted file")
     encrypted_file_name = input()
     filesize = getfilesizeinbytes(file_to_encrypt)
-    gen_random_key(filesize)
+    keytxt = gen_random_key(filesize)
+    createotpfile(keytxt, otp_file_name)
+    sourceinbinary = toBinary(gettxtfromsourcefile(file_to_encrypt))
+    #test code
+    print(sourceinbinary)
 
 
 def decryptfile():
+    """Function for dencrypt file"""
     print("Decryption............")
     print("Enter the filename to decrypt with extension")
     decrypted_file = input()
@@ -34,8 +40,35 @@ def find(name, path):
 
 
 def getfilesizeinbytes(filename):
+    """Get size of file"""
     # only hardcode on this case
     app_path = "D:\CyberSecurityEngineerViA\ActiveCryptography\PythonProgram\CryptoMaker"
     file_stats = os.stat(find(filename, app_path))
     print(f'File Size in Bytes is {file_stats.st_size}')
     return file_stats.st_size
+
+def createotpfile(key,filename):
+    """Create OTP file"""
+    file = open(filename,"w")
+    file.write(listtostr(str(key)))
+    file.close()
+
+def listtostr(list):
+    """Convert list to string"""
+    str = " "
+    return (str.join(list))
+
+def gettxtfromsourcefile(filename):
+    file = open(filename,"r")
+    #file.close()
+    return file
+
+def toBinary(text):
+    """Convert txt to binary"""
+    l,m=[],[]
+    #TODO: remove spaces,comas,and [] in text
+    for i in text:
+       l.append(ord(i))
+    for i in l:
+       m.append(int(bin(i)[2:]))
+    return m
